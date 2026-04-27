321e005ccc6cce364ae24a071528b8669f5161dd
Philipp Burlakov <fyuburlakov@edu.hse.ru>
2026-04-27 19:05:54 +0200

    Added VAE architectures (this time for sure)

---
+++ code/architectures/lm-decoding/vae_trainer.py
@@ -0,0 +1,224 @@
+from tqdm import tqdm
+import torch
+import evaluate
+
+bertscore = evaluate.load("bertscore")
+break_symbol = ' [IMAGE] '
+
+class VAEImageDescriptionTrainer:
+    def __init__(self, vae_encoder, trainable_tokens, qwen_model, qwen_tokenizer, image_adapter, device, lr=1e-4):
+        self.qwen_model = qwen_model
+        self.qwen_tokenizer = qwen_tokenizer
+        self.image_adapter = image_adapter
+        self.device = device
+        self.encoder = vae_encoder
+        self.trainable_tokens = trainable_tokens
+        self.encoder_dim = self.encoder.fc_mu.out_features // self.trainable_tokens
+        
+        break_tokens = self.qwen_tokenizer(break_symbol, return_tensors='pt')['input_ids'].to(self.device)
+        with torch.no_grad():
+            self.break_embeddings = qwen_model.model.embed_tokens(break_tokens).to(self.device)
+
+        for param in self.qwen_model.parameters():
+            param.requires_grad = False
+
+        self.optimizer = torch.optim.Adam(
+            [
+                {'params': self.image_adapter.parameters()},
+                {'params': self.encoder.parameters()}
+            ],
+            lr=lr
+        )
+    
+    def encode_texts(self, texts):
+        qwen_tokens = self.qwen_tokenizer(
+            texts,
+            return_tensors='pt',
+            padding='max_length',
+            max_length=64,
+            truncation=True
+        ).to(self.device)
+        
+        text_embeddings = self.qwen_model.model(**qwen_tokens)
+        
+        mu, log_var = self.encoder(text_embeddings)
+        
+        std = torch.exp(0.5 * log_var)
+        eps = torch.randn_like(mu, device=self.device)
+        z = eps * std + mu
+        return z.unflatten(1, (self.trainable_tokens, self.encoder_dim)), mu, log_var
+        
+    def get_loss(self, image_inputs, texts, val=False):
+        
+        noise_sampled, mu, log_var = self.encode_texts(texts)
+        
+        kld_loss = -0.5 * (1 + log_var - mu.pow(2) - log_var.exp()).mean()
+        
+        image_inputs = torch.concat(
+            [
+                image_inputs.to(self.device), 
+                noise_sampled
+            ],
+            dim=2
+        ).permute((0, 2, 1))
+        
+        image_embeddings = self.image_adapter(image_inputs).permute((0, 2, 1))
+
+        image_embeddings = torch.concat(
+            [
+              image_embeddings,
+              self.break_embeddings.detach().repeat(image_embeddings.shape[0], 1, 1)
+            ],
+            dim=1
+        )
+
+        qwen_tokens = self.qwen_tokenizer(
+            texts,
+            return_tensors='pt',
+            padding=True
+        ).to(self.device)
+
+        text_embeddings = self.qwen_model.model.embed_tokens(
+            qwen_tokens['input_ids']
+        )
+
+        all_embeddings = torch.cat(
+            [
+                image_embeddings,
+                text_embeddings
+            ],
+            dim=1
+        )
+
+        attn_mask = torch.cat(
+            [
+                torch.ones(image_embeddings.shape[:2], device=self.device),
+                qwen_tokens['attention_mask']
+            ],
+            dim=1
+        )
+
+        targets = torch.cat(
+            [
+                torch.full(image_embeddings.shape[:2], -100, device=self.device),
+                torch.where(
+                    qwen_tokens['attention_mask']==1,
+                    qwen_tokens['input_ids'],
+                    -100
+                )
+            ],
+            dim=1
+        ).to(torch.int64)
+
+        loss = self.qwen_model(
+            inputs_embeds=all_embeddings,
+            attention_mask=attn_mask,
+            labels=targets
+        ).loss + kld_loss
+        
+        if not val:
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
+            
+        return loss.item()
+
+    def run_epoch(self, loader, val=False):
+        total_loss = 0.0
+        total_examples = 0
+
+        if val:
+            self.image_adapter.eval()
+            self.encoder.eval()
+            with torch.no_grad():
+                for batch in tqdm(loader, desc="Validating"):
+                    image_inputs, texts = batch
+                    total_loss += self.get_loss(image_inputs, texts, val=True) * image_inputs.size(0)
+                    total_examples += image_inputs.size(0)
+
+        else:
+            self.image_adapter.train()
+            self.encoder.train()
+            for batch in tqdm(loader, desc="Training"):
+                image_inputs, texts = batch
+                total_loss += self.get_loss(image_inputs, texts, val=False) * image_inputs.size(0)
+                total_examples += image_inputs.size(0)
+                
+        torch.cuda.empty_cache()
+        return total_loss / total_examples
+
+    def generate(self, image_embeddings, max_tokens=20):
+        self.image_adapter.eval()
+        self.encoder.eval()
+        with torch.no_grad():
+            noise_sampled = torch.randn((
+                image_embeddings.shape[0],
+                self.trainable_tokens,
+                self.encoder_dim
+            ))
+            
+            image_inputs = torch.concat(
+                [
+                    image_inputs.to(self.device), 
+                    noise_sampled
+                ],
+                dim=2
+            ).permute((0, 2, 1))
+            
+            
+            previous_embeddings = self.image_adapter(image_inputs).permute((0, 2, 1))
+    
+            previous_embeddings = torch.concat(
+                [
+                  previous_embeddings,
+                  self.break_embeddings.detach().repeat(previous_embeddings.shape[0], 1, 1).to(self.device)
+                ],
+                dim=1
+            )
+    
+            attn_mask = torch.ones(previous_embeddings.shape[:2], device=self.device)
+    
+            fake_input_ids = torch.full(
+                previous_embeddings.shape[:2],
+                self.qwen_tokenizer.pad_token_id,
+                device=self.device
+            ).to(torch.int64)
+    
+            generated_ids = self.qwen_model.generate(
+                input_ids=fake_input_ids,
+                inputs_embeds=previous_embeddings,
+                attention_mask=attn_mask,
+                max_new_tokens=max_tokens,
+                do_sample=False,
+                eos_token_id=self.qwen_tokenizer.eos_token_id,
+            )
+    
+            gen_text_ids = generated_ids[:, previous_embeddings.shape[1]:]
+
+        return self.qwen_tokenizer.batch_decode(
+            gen_text_ids,
+            skip_special_tokens=True
+        )
+    
+    def evaluate(self, loader):
+        out_df = {
+            'references': [],
+            'predictions': [],
+            'bert_scores': []
+        }
+
+        for batch in tqdm(loader, desc="Testing"):
+            image_inputs, references = batch
+            predictions = self.generate(image_embeddings=image_inputs, max_tokens=100)
+            bert_scores = bertscore.compute(
+                predictions=predictions,
+                references=references,
+                device=self.device,
+                lang="en"
+            )['f1']
+            
+            out_df['references'].extend(references)
+            out_df['predictions'].extend(predictions)
+            out_df['bert_scores'].extend(bert_scores)
+
+        return out_df
---
+++ code/architectures/vae.py
@@ -0,0 +1,43 @@
+import torch
+from torch import nn
+
+class VAEnc(nn.Module):
+    def __init__(
+        self,
+        input_dim: int,
+        output_dim: int,
+        hidden_dim: int = 1024,
+        input_tokens: int = 100,
+        kernel_size = 5,
+        kernel_stride = 5,
+        layers_num: int = 3,
+    ) -> None:
+
+        super().__init__()
+        
+        nn_layers = []
+        output_shape = input_tokens
+        for i in range(layers_num):
+            nn_layers.extend([
+                nn.Conv1d(
+                    input_dim if i == 0 else hidden_dim,
+                    hidden_dim,
+                    kernel_size,
+                    kernel_stride
+                ),
+                nn.ReLU()
+            ])
+            output_shape = int((output_shape - kernel_size) / kernel_stride) + 1
+
+        nn_layers.append(nn.Flatten())
+
+        self.encoder = nn.Sequential(*nn_layers)
+        
+        self.fc_mu = nn.Linear(output_shape * hidden_dim, output_dim)
+        self.fc_var = nn.Linear(output_shape * hidden_dim, output_dim)
+
+    def forward(self, x: torch.Tensor):
+        x = self.encoder(x)
+        mu = self.fc_mu(x)
+        log_var = self.fc_var(x)
+        return mu, log_var
